1fea9193c465830bd7ac053d214aa2feee1318d0
Yashraj Suman <98071360+Itsyashraj@users.noreply.github.com>
2023-07-08 17:10:19 +0530

    Update EvenOdd.java

--- Bit-Manupulation/EvenOdd.java
+++ Bit-Manupulation/EvenOdd.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class EvenOdd {
+
+  public boolean checkEven(int n) {
+
+    return ((n & 1) == 0);
+  }
+
+  Public static void main (String [] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+
+    if(checkEven(num)
+       System.out.println("Even");
+    else
+      System.out.println("Odd");
+  }
+}
